99a8365a813d9c6e365b5d1d0e7e6f88699b9083
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-10-23 15:30:04 -0400

    new repo

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create(title: 'Raunty', content: 'Rails/JS Project', github_link: 'https://github.com/Tanzi11/raunty', image_link: 'https://res.cloudinary.com/df4bkz6cu/image/upload/v1571168411/samples/animals/reindeer.jpg')
+Project.create(title: 'Alltop', content: 'CLI Project', github_link: 'https://github.com/Tanzi11/alltop',image_link: 'https://res.cloudinary.com/df4bkz6cu/image/upload/v1571168417/samples/ecommerce/accessories-bag.jpg')
